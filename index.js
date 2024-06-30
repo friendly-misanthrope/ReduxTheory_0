53b1632e8b2458d2f7342015c45032f632a2dca5
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-29 18:50:28 -0700

    Action types, reducer function

---
+++ index.js
@@ -0,0 +1,25 @@
+// An action is an object with a type property
+// An action creator is a function that returns an action object:
+const CAKE_ORDERED = 'CAKE_ORDERED';
+
+const orderCake = () => {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1
+  }    
+}
+
+// A reducer is a function that accepts current state and an action object,
+// and returns the new state:
+const initialState = {
+  numCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+  if (action.type === 'CAKE_ORDERED'){
+    return {
+      numCakes: state.numCakes --
+    }
+  }
+  return state;
+}
