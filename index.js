9a1f2ef76eba5725a2dc935c72d7487d93b201e3
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-29 19:11:30 -0700

    created a store that accepts the reducer function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // An action is an object with a type property
 // An action creator is a function that returns an action object:
+const redux = require('redux');
+const createStore = redux.createStore;
 const CAKE_ORDERED = 'CAKE_ORDERED';
 
 const orderCake = () => {
@@ -25,3 +27,15 @@ const reducer = (state = initialState, action) => {
   }
   return state;
 }
+
+
+// It exposes a method called getState(),
+// allows state to be updated via dispatch(action), and
+// registers event listeners via subscribe() that
+// is called whenever state changes.
+// You can also unsubscribe from the store by calling the function
+// that was returned by the subscribe method.
+
+// The store is where all global application state is stored.
+// createStore accepts the reducer function as an argument.
+const store = createStore(reducer)
