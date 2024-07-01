97e52bf237d14d82d3e1c01cf70165510b66c09f
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-30 21:48:29 -0700

    Created file for updating state using Immer. Added initialState.

---
+++ nestedState.js
@@ -0,0 +1,13 @@
+// Using Immer.js to update state without mutating it
+const redux = require('redux');
+
+// define initial state
+const initialState = {
+  name: 'Nick',
+  address: {
+    street: '1929 181st Street SE',
+    city: 'Bothell',
+    state: 'WA'
+  }
+}
+
