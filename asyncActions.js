70925317738c67bc929c4d2a69e2296df9790898
friendly-misanthrope <nickthedevguy@gmail.com>
2024-06-30 23:34:21 -0700

    Added request/success/failure action types and creators

--- asyncActions.js
+++ asyncActions.js
@@ -5,3 +5,27 @@ const initialState = {
   users: [],
   error:''
 }
+
+const FETCH_USERS_REQUESTED = 'FETCH_USERS_REQUESTED'
+const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS'
+const FETCH_USERS_FAILED = 'FETCH_USERS_FAILED'
+
+const fetchUsersRequested = () => {
+  return {
+    type: FETCH_USERS_REQUESTED
+  }
+}
+
+const fetchUsersSuccess = (users) => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users
+  }
+}
+
+const fetchUsersFailed = (error) => {
+  return {
+    type: FETCH_USERS_FAILED,
+    payload: error
+  }
+}
